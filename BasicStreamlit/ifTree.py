64639f6ac36fbdf8fe296a536ec112d58235d94f
edsaa <esaavedrac@u.northwestern.edu>
2022-08-12 07:16:01 -0500

    a bunch of examples

--- BasicStreamlit/ifTree.py
+++ BasicStreamlit/ifTree.py
@@ -23,32 +23,3 @@ else:
     cb3 = st.sidebar.checkbox("Func 3",key=6)
 
 run_btn = st.sidebar.button('Run', on_click=execute, key='e')
-
-## Does not work
-# import streamlit as st
-
-# def func1(): st.write("Function 1 executed.")
-# def func2(): st.write("Function 2 executed.")
-# def func3(): st.write("Function 3 executed.")
-
-# # function to run chosen function(s)
-# def execute():
-#     if multiselect:
-#         for i,func in enumerate(functionList):
-#             if multiselect[i]: func()
-#     else:
-#         st.warning(":warning: No functions were selected")
-
-# # checkbox shenanigans 
-# cbAll = st.sidebar.checkbox("Select All")
-
-# optionList = ["Function 1","Function 2","Function 3"]
-# functionList = [func1,func2,func3]
-
-# if cbAll:
-#     multiselect = st.sidebar.multiselect("Select functions",options=optionList,default=optionList,disabled=True,key=2)
-
-# else:
-#     multiselect = st.sidebar.multiselect("Select functions",options=optionList,default=None,key=1)
-
-# run_btn = st.sidebar.button('Run', on_click=execute, key='e')
--- BasicStreamlit/readExcel.py
+++ BasicStreamlit/readExcel.py
@@ -3,21 +3,6 @@ import streamlit as st
 import pandas as pd
 import openpyxl
 
-# st.subheader("Dataset")
-# data_file = st.file_uploader("Upload excel",type=['xlsx'])
-# process_btn = st.button("Process",on_click=readExcelFile)
-
-# if data_file is not None:
-#     file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-#     st.write(file_details)
-#     wb = openpyxl.load_workbook(data_file)
-#     sheet_selector = st.selectbox("Select sheetname",wb.sheetnames)     
-#     st.markdown(f"# Currently Selected {sheet_selector}")
-#     df = pd.read_excel(data_file,sheet_name=sheet_selector)
-#     st.write(df)
-
-# Nesting widgets after a button is not a good idea because it immediately goes back to a False state (There is a demo of that behavior in this video tutorial: https://youtu.be/EnXJBsCIl_A).
-
 st.header("Sheet view")
 data_file = st.sidebar.file_uploader("Upload Excel file",type=['xlsx'])  
 
