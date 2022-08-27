6cda5a46847cbc6f8bd80b1aa05c8825abe068b9
edsaa <esaavedrac@u.northwestern.edu>
2022-08-26 19:16:09 -0500

    idk

--- BasicStreamlit/insertSTL.py
+++ BasicStreamlit/insertSTL.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pyvista as pv
-from pyvista import examples
+import io
+import tempfile
 
 ## Using pythreejs as pyvista backend
 pv.set_jupyter_backend('pythreejs')
 
-## Download an stl file
-# filename = examples.download_cad_model(load=False)
+## Upload a pyvista file
 uploadedFile = st.file_uploader("Upload a STL:",["stl"],False)
 
 ## Streamlit layout
@@ -14,30 +14,28 @@ st.sidebar.title("STL viewer")
 
 if uploadedFile:
     
-    stlTemp = "./temp.stl"
-    with open(stlTemp, "wb") as f: 
-        f.write(uploadedFile.getbuffer())
-
-    color = st.sidebar.selectbox("Pick a color:",["white","green","blue"])
+    ## Color panel
+    col1,col2 = st.sidebar.columns(2)
+    color_stl = col1.color_picker("Element","#0BD88D")
+    color_bkg = col2.color_picker("Background","#FFFFFF")
 
     ## Initialize pyvista reader and plotter
-    reader = pv.STLReader(stlTemp)
-    plotter = pv.Plotter(
-        border=True,
-        window_size=[580,400]) 
-    plotter.background_color = "white"
+    plotter = pv.Plotter(border=False, window_size=[500,400]) 
+    plotter.background_color = color_bkg
 
-    ## Read data and send to plotter
-    mesh = reader.read()
-    plotter.add_mesh(mesh,color=color)
+    ## Create a tempfile to keep the uploaded file as pyvista's API 
+    ## only supports file paths but not buffers
+    with tempfile.NamedTemporaryFile(suffix="_streamlit") as f: 
+        f.write(uploadedFile.getbuffer())
+        reader = pv.STLReader(f.name)
+    
+        ## Read data and send to plotter
+        mesh = reader.read()
+        plotter.add_mesh(mesh,color=color_stl)
 
-    ## Export to an external pythreejs
-    model_html = "model.html"
-    other = plotter.export_html(model_html, backend='pythreejs')
-
-    ## Read the exported model
-    with open(model_html,'r') as file: 
-        model = file.read()
-
-    ## Show in webpage
-    st.components.v1.html(model,height=500)
+        ## Export to a pythreejs HTML
+        model_html = io.StringIO()
+        plotter.export_html(model_html, backend='pythreejs')
+        
+        ## Show in webpage
+        st.components.v1.html(model_html.getvalue(),height=400)
