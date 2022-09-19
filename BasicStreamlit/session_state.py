9ef4286df08a6cd7e230119bce3cfc7ca1d5f278
Ed Saa <esaavedrac@u.northwestern.edu>
2022-09-18 19:38:35 -0500

    moved basic streamlit to other repo

--- BasicStreamlit/session_state.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-if 'button_1' not in st.session_state:
-    st.session_state.button_1 = False
-if 'button_2' not in st.session_state:
-    st.session_state.button_2 = False
